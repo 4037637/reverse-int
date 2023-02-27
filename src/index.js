cda454d187c8524b33d8739059f7ca0fb635b088
4037637 <4037637@gmail.com>
2023-02-27 16:46:39 +0100

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    n += ''
+    let reversed = ''
+    for (let i = 0; i < n.length; i++) {
+        let oneEl = n[n.length - 1 - i]
+        reversed += oneEl
+    }
+    return reversed
 }
